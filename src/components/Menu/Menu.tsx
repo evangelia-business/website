58e35b30d85cefc98fbbac715ec3dd51a39079f5
Evangelia Mitsopoulou <contact@evangelia.me>
2024-07-22 01:34:10 +0300

    #3 install talwind and use its utitility and base classes, remove modern-normalize, made logo smaller

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -7,7 +7,7 @@ export default function Menu() {
   return (
 
     <Box as="header" className={styles.header}>
-      <HStack spacing="1rem">
+      <HStack as="nav" spacing="1rem">
         <Link as={NextLink} href='https://www.evangelia.me' className={styles.menuitem}>Home</Link>
         <Link as={NextLink} href='https://tech.evangelia.me' className={styles.menuitem}>Philomath</Link>
       </HStack>

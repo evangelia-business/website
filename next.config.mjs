815d2f8809e4d1a67ef348f7b7d000a7625788e4
Evangelia Mitsopoulou <contact@evangelia.me>
2024-07-26 17:49:18 +0300

    #10 fix formatting next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // Specify the custom directory structure
-  pageExtensions: ['js', 'jsx', 'ts', 'tsx']
+  pageExtensions: ['js', 'jsx', 'ts', 'tsx'],
 }
 
 export default nextConfig

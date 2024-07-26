02d56825e0417a8001dd4e0b3731c8cd018cd6a6
Evangelia Mitsopoulou <contact@evangelia.me>
2024-07-26 17:19:42 +0300

    #10 format:fix next.config.njs

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    // Specify the custom directory structure
-    pageExtensions: ['js', 'jsx', 'ts', 'tsx'],
-    experimental: {
-        appDir: true,
-    },
-    // Add the basePath property
-    basePath: '/src',
+  // Specify the custom directory structure
+  pageExtensions: ['js', 'jsx', 'ts', 'tsx'],
+  experimental: {
+    appDir: true,
+  },
+  // Add the basePath property
+  basePath: '/src',
 }
 
 export default nextConfig

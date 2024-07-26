f072aec375d163681e68c884462b969e6e091873
Evangelia Mitsopoulou <contact@evangelia.me>
2024-07-26 16:54:48 +0300

    #10 added precommit checks:unused exports, typescript type error checks

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    // Specify the custom directory structure
+    pageExtensions: ['js', 'jsx', 'ts', 'tsx'],
+    experimental: {
+        appDir: true,
+    },
+     // Add the basePath property
+     basePath: '/src',
+}
 
 export default nextConfig

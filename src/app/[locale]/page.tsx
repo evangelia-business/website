cafb83e8c6d2b675e86063fb6a21cb5d2d0df47f
Administrator <admin@udecide.ch>
2024-08-08 11:48:29 +0200

    Update localization settings and adjust tests for i18n integration

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,8 +1,5 @@
-import { useTranslations } from 'next-intl'
-
 import HomePage from '../../components/HomePage/HomePage'
 export default function Home() {
-  const t = useTranslations('HomePage')
   return (
     <main>
       <HomePage />

9d65cf33734a3e0f10dd307510e659e7c078583f
Evangelia Mitsopoulou <contact@evangelia.me>
2024-08-13 10:51:42 +0300

    #9 save changes for previous commit

--- src/app/[locale]/providers.tsx
+++ src/app/[locale]/providers.tsx
@@ -1,10 +1,26 @@
 // app/providers.tsx
 'use client'
 
-import theme from '../styles/settings/theme'
+import theme from '@/styles/settings/theme'
+import { NextIntlClientProvider } from 'next-intl'
 
 import { ChakraProvider } from '@chakra-ui/react'
 
-export function Providers({ children }: { children: React.ReactNode }) {
-  return <ChakraProvider theme={theme}>{children}</ChakraProvider>
+type ProvidersProps = {
+  children: React.ReactNode
+  messages: Record<string, unknown>
+  locale: string
+}
+export function Providers({ children, messages, locale }: ProvidersProps) {
+  return (
+    <ChakraProvider theme={theme}>
+      <NextIntlClientProvider
+        messages={messages}
+        locale={locale}
+        timeZone="Europe/Athens"
+      >
+        {children}
+      </NextIntlClientProvider>
+    </ChakraProvider>
+  )
 }

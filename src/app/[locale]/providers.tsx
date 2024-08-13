11599de1f8cd62856ae1dacfb35639fb51037f45
Evangelia Mitsopoulou <contact@evangelia.me>
2024-08-13 10:48:56 +0300

    #9 fix git mv history for providers

--- src/app/[locale]/providers.tsx
+++ src/app/[locale]/providers.tsx
@@ -1,26 +1,10 @@
 // app/providers.tsx
 'use client'
 
-import theme from '@/styles/settings/theme'
-import { NextIntlClientProvider } from 'next-intl'
+import theme from '../styles/settings/theme'
 
 import { ChakraProvider } from '@chakra-ui/react'
 
-type ProvidersProps = {
-  children: React.ReactNode
-  messages: Record<string, unknown>
-  locale: string
-}
-export function Providers({ children, messages, locale }: ProvidersProps) {
-  return (
-    <ChakraProvider theme={theme}>
-      <NextIntlClientProvider
-        messages={messages}
-        locale={locale}
-        timeZone="Europe/Athens"
-      >
-        {children}
-      </NextIntlClientProvider>
-    </ChakraProvider>
-  )
+export function Providers({ children }: { children: React.ReactNode }) {
+  return <ChakraProvider theme={theme}>{children}</ChakraProvider>
 }

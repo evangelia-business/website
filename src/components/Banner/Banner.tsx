1d13c43e13e35d4b8d61598688059eca515dec27
Evangelia Mitsopoulou <contact@evangelia.me>
2024-10-01 13:31:05 +0300

    #5 removed further attributes like paddingbottoms, |

--- src/components/Banner/Banner.tsx
+++ src/components/Banner/Banner.tsx
@@ -17,12 +17,15 @@ const Banner: FC<BannerProps> = ({ onClick }) => {
   return (
     <Grid templateColumns={'1'} className={styles.bannerContainer}>
       <GridItem>
-        <Box className={styles.titleRight} margin="0 auto" right={0}>
+        <Box
+          className={styles.titleRight}
+          margin="0 auto"
+          right={0}
+          textAlign="center"
+        >
           <Text
             width="auto"
             fontSize={['2.3rem', '2.4rem', '2.6rem', '2.8rem']}
-            textAlign="center"
-            paddingTop={['0.5rem', '0.5rem', '0', '0']}
             paddingBottom={['1.3rem', '1.3rem', '1rem', '1rem']}
             lineHeight={['2.5rem', '2.5rem', '2.2rem', '2.6rem']}
             fontWeight={700}
@@ -32,8 +35,6 @@ const Banner: FC<BannerProps> = ({ onClick }) => {
           <Text
             width="auto"
             fontSize={['1.5rem', '1.6rem', '1.7rem', '1.9rem']}
-            textAlign="center"
-            paddingBottom={['1.3rem', '1.3rem', '0', '0']}
             lineHeight={['1.4rem', '1.4rem', '2.2rem', '2.6rem']}
             fontWeight={400}
           >
